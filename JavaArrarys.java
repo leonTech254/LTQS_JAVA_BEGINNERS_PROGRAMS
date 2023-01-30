936a922e92928b9853d198dfa5ce8ae01958dd38
leontech <martinleontech23@gmail.com>
2023-01-30 17:08:55 +0300

    --arraays and ArrayLists

---
+++ JavaArrarys.java
@@ -0,0 +1,24 @@
+package leonteqsecurity;
+import java.util.ArrayList;
+
+public class JavaArrarys {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] numbers= {1,2,3,4,5,6,7};
+		String[] fruits= {"Mango","Orange"};
+		System.out.println(numbers[4]);
+//		ARRAY LIST
+		ArrayList<Integer> numberOfStudent=new ArrayList<Integer>();
+		numberOfStudent.add(3);
+		numberOfStudent.add(4);
+		numberOfStudent.add(1);
+		numberOfStudent.add(7);
+		
+		System.out.println(numberOfStudent.get(0));
+		
+		
+
+	}
+
+}
