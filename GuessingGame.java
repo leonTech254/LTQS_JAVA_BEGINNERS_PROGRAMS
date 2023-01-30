c074815466d927cc3482cdeff8ec92722e556a18
leontech <martinleontech23@gmail.com>
2023-01-30 12:01:33 +0300

    --guessing game

---
+++ GuessingGame.java
@@ -0,0 +1,55 @@
+package leonteqsecurity;
+import java.util.Scanner;
+
+public class GuessingGame {
+	
+	public static int Random()
+	{
+		int Randomnumbers=(int)(Math.random()*100+1);
+		
+		return Randomnumbers;
+		
+	}
+	
+	public static void Game()
+	{
+		int Generated=Random();
+		System.out.println("GUESS A NUMBER BETWEEN 1-100");
+		Scanner sc= new Scanner(System.in);
+		int UserInput=sc.nextInt();
+		System.out.println(UserInput);
+//		sc.close();
+//		This condation checks if the System number is correct with the user Guess number
+		if (Generated==UserInput)
+		{
+			System.out.println("Correct! you won the Gessing game challange");
+			
+		}
+		else
+		{
+			System.out.println("Guess Failed the correct number is "+ Generated);
+			System.out.println("DO YOU WANT TO PLAY AGAIN? (1=Yes,0=No)");
+			int UserResponse=sc.nextInt();
+			if (UserResponse==1)
+			{
+				Game();
+				
+			}else
+			{
+				System.out.println("GAME OVER....EXITED!");
+			}
+				
+			
+			
+			
+		}
+	}
+	
+
+	public static void main(String[] args) {
+			Game();
+		
+
+	}
+
+}
---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+package leonteqsecurity;
+import java.util.Scanner;
+
+public class HelloWorld {
+	public static int school(int a,int b)
+	{
+       int sum=a+b;
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("HELL WORLD");
+	
+	}
+	
+
+}
